59bdfcd8f0a271bd51f6193d25d42bcfbd6d3a93
Manshi Sanghai <manshisanghai10@gmail.com>
2017-04-19 23:09:35 +0530

    Update merge-parallel.cpp
    Comments added, actions documented.

--- mergeSort/merge-parallel.cpp
+++ mergeSort/merge-parallel.cpp
@@ -1,15 +1,22 @@
 //merge sort parallel
+//compile with -fopenmp flag 
+//to calculate time taken on linux, use "time" keyword while executing object file
+
 #include<iostream>
-#include<omp.h>
+#include "omp.h"
 using namespace std;
+//performs the bottom up combine operation
 void merge(int a[], int l, int m, int r)
 {
 	int temp[m-l+1], temp2[r-m];
+	//copy first part to temporary array
 	for(int i=0; i<(m-l+1); i++)
 		temp[i]=a[l+i];
+	//copy second part to temporary arrat]y
 	for(int i=0; i<(r-m); i++)
 		temp2[i]=a[m+1+i];
 	int i=0, j=0, k=l;
+	//combine the arrays on the basis of order
 	while(i<(m-l+1) && j<(r-m))
 	{
 		if(temp[i]<temp2[j])
@@ -17,13 +24,19 @@ void merge(int a[], int l, int m, int r)
 		else
 			a[k++]=temp2[j++];
 	}
-	
+	//to combine the remainder of the two arrays
 	while(i<(m-l+1))
 		a[k++]=temp[i++];
 	while(j<(r-m))
 		a[k++]=temp2[j++];
 
 }
+/* 
+This function is the main hotspot of the algorithm which I have chosen to parallelise
+The calls labelled call 1 and call 2 are essentially independent since they are operating on distinct
+parts of the array and hence can be called in parallel.
+*/
+
 void mergeSort(int a[], int l, int r)
 {
 	if(l<r)
@@ -34,16 +47,17 @@ void mergeSort(int a[], int l, int r)
 		{
 			#pragma omp section
 			{
-				mergeSort(a,l,m);
+				mergeSort(a,l,m); //call 1
 			}
 			#pragma omp section
 			{
-			mergeSort(a,m+1,r);
+				mergeSort(a,m+1,r); //call 2
 			}
 		}
-		merge(a,l,m,r);
+		merge(a,l,m,r); //this function is outside the parallel sections since it merges and access the same data 
 	}
 }
+//a simple print function to print all the elements
 void print(int a[], int n)
 {
 	for(int i=0; i<n; i++)
@@ -59,6 +73,7 @@ int main()
 		cin>>a[i];
 	cout<<"\nUnsorted array; ";
 	print(a,n);
+	//call should be to a wrapper function that calls the mergeSort function
 	mergeSort(a,0,n-1);
 	cout<<"\nSorted array: ";
 	print(a,n);
